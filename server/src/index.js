5e81724feae41149fbf8aa2c7728b9475553bae7
Salva Carsi <salvavcarsi@gmail.com>
2017-08-02 12:31:35 +0200

    using body parser

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import logger from 'morgan';
 import mongodb from 'mongodb';
+import bodyParser from 'body-parser';
 
 const app = express();
 const MongoClient = mongodb.MongoClient;
@@ -8,14 +9,21 @@ const MongoClient = mongodb.MongoClient;
 const url = 'mongodb://localhost:27017/my-app';
 
 app.use(logger('dev'));
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: false
+}));
 
 app.post('/register', (req, res) => {
     MongoClient.connect(url, function(err, db) {
         var users = db.collection('users');
-        users.insertOne({ name: "salva", password: "test1234" });
+        users.insertOne({ email: req.body.email, password: req.body.password });
         db.close();
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ message: 'user inserted' }));
+        res.send(JSON.stringify({
+            message: 'registration successfull',
+            email: req.body.email,
+        }));
     });
 });
 
