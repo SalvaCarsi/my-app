d13af9748d263a7d3be0339d4fcf2992bd43b954
Salva Carsi <salvavcarsi@gmail.com>
2017-08-03 12:32:39 +0200

    using bcrypt for encrypting the password

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,27 +1,31 @@
 import express from 'express';
 import mongodb from 'mongodb';
+import bcrypt from 'bcrypt';
 import { mongo_url } from '../config.js';
 
 const router = express.Router();
 const MongoClient = mongodb.MongoClient;
 
 router.post('/register', (req, res) => {
-    MongoClient.connect(mongo_url, (err, db) => {
+    MongoClient.connect(mongo_url).then((db) => {
         const users = db.collection('users');
-        users.findOne({ email: req.body.email }, (err, doc) => {
-            res.setHeader('Content-Type', 'application/json');
-            if (doc === null) {
-                users.insertOne({ email: req.body.email, password: req.body.password });
-                res.send({
-                    message: 'auth.register.success',
-                    email: req.body.email,
-                });
-            } else {
+        users.findOne({ email: req.body.email }).then((doc) => {
+            if (doc !== null) {
+                db.close();
                 res.send({ message: 'auth.register.error.user.exist' });
             }
-            db.close();
+            else {
+                bcrypt.hash(req.body.email, 5).then((hash) => {
+                    users.insertOne({ email: req.body.email, password: hash });
+                    db.close()
+                    res.send({
+                        message: 'auth.register.success',
+                        email: req.body.email,
+                    });
+                });
+            }
         });
-    });
+    }).catch((error) => console.log(error));
 });
 
 export default router;
