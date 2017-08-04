600cc209a977185c7b2ac0e810eb511d71eb7aa8
Salva Carsi <salvavcarsi@gmail.com>
2017-08-04 13:50:56 +0200

    simplifying code

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -9,31 +9,28 @@ mongoose.connect(MONGO_URL, { useMongoClient: true });
 
 const router = express.Router();
 
+const dealWithError = (error, res, messageId) => {
+    console.log(`error: ${error}`);
+    res.json({
+        messageId,
+        error: error,
+    });
+}
+
 router.post('/register', (req, res) => {
     bcrypt.hash(req.body.password, BCRYPT_ROUNDS).then((encryptedPassword) => {
-        const newUser = new User({
+        new User({
             name: req.body.name,
             email: req.body.email,
             password: encryptedPassword,
-        });
-        newUser.save().then((doc) => {
-                console.log(doc);
-                console.log('here we are!');
-                res.send({
-                    messageId: 'auth.register.success',
-                    email: req.body.email,
-                });
-            }).catch((error) => {
-                console.log(`error: ${error}`);
-                res.send({
-                    messageId: 'auth.register.error',
-                    error: error,
-                });
-            })
-    }).catch((error) => {
-        console.log(`error: ${error}`);
-        res.send(error);
-    });
+        }).save().then(() => {
+            console.log(`user with email ${req.body.email} has been registered`);
+            res.json({
+                messageId: 'auth.register.success',
+                email: req.body.email,
+            });
+        }).catch( error => dealWithError(error, res, 'auth.register.error'));
+    }).catch( error => dealWithError(error, res, 'auth.register.error'));
 });
 
 export default router;
